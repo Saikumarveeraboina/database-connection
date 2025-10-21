c991614452b5582f9817c3cb7e36c2b1300a3f34
Saikumarveeraboina <saikumar96030433@gmail.com>
2025-10-21 20:03:38 +0530

    added filess

---
+++ db.js
@@ -0,0 +1,28 @@
+const app = require('./index.js');
+const dotenv = require('dotenv')
+
+dotenv.config()
+//simple route
+app.get('/', (req, res) =>{
+    res.send("Hello World")
+})
+
+//Connecting to database
+const mysql2 = require('mysql2');
+
+const mysql = mysql2.createConnection({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASS,
+    database: process.env.DATABASE,
+
+})
+mysql.connect((err, data) => {
+    if (err) {
+        console.log("Error while connecting to database", err);
+    }
+    else {
+        console.log("Database connected successfully");
+    }
+    
+})

47872db91342fb4c8fd37b087ac37fdd0de2a5a0
Saikumar <saikumar96030433@gmail.com>
2025-12-09 20:44:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
-
 //importig dotenv
 dotenv.config();
 const PORT = process.env.PORT || 3000; 
-
 //crating a server 
 app.listen(PORT, ()=>{
     console.log("Server is running at port", PORT);
